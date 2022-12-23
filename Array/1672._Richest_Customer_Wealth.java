64a47000b5a5bc2487f320fb285eae63a55729c3
Mohit <80634689+Mohit030802@users.noreply.github.com>
2022-12-23 13:52:35 +0530

    Create 1672._Richest_Customer_Wealth.java

---
+++ Array/1672._Richest_Customer_Wealth.java
@@ -0,0 +1,17 @@
+/* https://leetcode.com/problems/richest-customer-wealth/ */
+
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int ans=Integer.MIN_VALUE;
+        for (int[] ints : accounts) {
+            int sum = 0;
+            for (int anInt : ints) {
+                sum += anInt;
+            }
+            if (sum > ans) {
+                ans = sum; 
+            }
+        }
+        return ans;
+    }
+}
